fc45cf5d746ff5e3573483a2972fd54a09c0072b
Peter Goeren <peter.goeren@gmail.com>
2019-03-12 21:08:39 -0600

    Added an augmented division /= to shorthand the division of a value to a variable the user has defined

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,11 @@
+//I learned that Javascript can use an augmented division /= to shorthand the division of a value to a variable the user has defined
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
