05b994be5bfd6ee820731feff77658be2c231863
Peter Goeren <peter.goeren@gmail.com>
2019-03-12 20:37:13 -0600

    i-- is an increment operator that can be applied to any variable. Subtracts one from the value of the variable.

---
+++ 5-Introduction-to-JavaScript/12-Decrement-a-Number-with-JavaScript.js
@@ -0,0 +1,6 @@
+//I learned that i-- is an increment operator that can be applied to any variable. Subtracts one from the value of the variable.
+
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
