50427385af80f548e6da630b864ecabfdacbe829
Peter Goeren <peter.goeren@gmail.com>
2019-03-12 20:54:09 -0600

    Added an augmented addition += to shorthand a variable that adds a number to itself

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,11 @@
+//I learned that Javascript can use an augmented addition += to shorthand an addition to a variable adding to itself.
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
