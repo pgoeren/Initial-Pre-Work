7f148356bde1859052ccd1afdc705ca2bed573b7
Peter Goeren <peter.goeren@gmail.com>
2019-03-12 21:03:23 -0600

    Used an augmented subtraction -= to shorthand a subtraction to a variable subtracting from itself.

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,11 @@
+//I learned that Javascript can use an augmented subtraction -= to shorthand a subtraction to a variable subtracting from itself.
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
