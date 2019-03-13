63bf15c8cdffa0eef0213ae6b27ec8a5fd7ee717
Peter Goeren <peter.goeren@gmail.com>
2019-03-12 20:11:36 -0600

    Applied standard naming conventions of camelCase for best practices

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,11 @@
+//I learned about standard naming conventions of camelCase and best practices
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
