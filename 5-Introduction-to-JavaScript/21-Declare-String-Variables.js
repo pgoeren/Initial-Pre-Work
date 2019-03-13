b4fb86a65359acf9b937453332e4a48784665533
Peter Goeren <peter.goeren@gmail.com>
2019-03-12 21:11:06 -0600

    Created a string literal in Javascript

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,10 @@
+//I learned about how to create a string literal in Javascript
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Peter"
+var myLastName = "Goeren"
