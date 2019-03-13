c94688d142e2c5bb1d26a5e94b3da86c5c5e08d2
Peter Goeren <peter.goeren@gmail.com>
2019-03-13 12:39:26 -0600

    duplicate

--- 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -1,3 +1,3 @@
-//I learned about how to escape a string literal using backslash in front of quotes
+//I learned about how to escape a string using backslash in front of quotes
 
 var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
